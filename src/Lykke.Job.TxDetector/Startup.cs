fe00d06063cea8f3ddc424125e2f6bdf778665d1
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-12-22 00:29:40 +0300

    Updated nuget packages references.

--- src/Lykke.Job.TxDetector/Startup.cs
+++ src/Lykke.Job.TxDetector/Startup.cs
@@ -98,7 +98,11 @@ namespace Lykke.Job.TxDetector
 
             app.UseMvc();
             app.UseSwagger();
-            app.UseSwaggerUi();
+            app.UseSwaggerUI(x =>
+            {
+                x.RoutePrefix = "swagger/ui";
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
             app.UseStaticFiles();
 
             appLifetime.ApplicationStarted.Register(Start);
