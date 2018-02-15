ae84071c818b5b750bed04dd9bf2d9be5159e515
Roman Chuprikov <paradise.develop@gmail.com>
2018-02-15 12:07:28 +0500

    fix NRE

--- src/Lykke.Job.TxDetector.Services/BitCoin/Ninja/NinjaUtils.cs
+++ src/Lykke.Job.TxDetector.Services/BitCoin/Ninja/NinjaUtils.cs
@@ -20,16 +20,16 @@ namespace Lykke.Job.TxDetector.Services.BitCoin.Ninja
                 ReceivedCoins = item.ReceivedCoins.Select(
                     x => new InputOutput
                     {
-                        Address = x.TxOut.ScriptPubKey.GetDestinationAddress(network).ToString(),
+                        Address = x.TxOut.ScriptPubKey.GetDestinationAddress(network)?.ToString(),
                         Amount = (x as ColoredCoin)?.Amount.Quantity ?? ((Coin)x).Amount.Satoshi,
-                        BcnAssetId = (x as ColoredCoin)?.AssetId.ToString()
+                        BcnAssetId = (x as ColoredCoin)?.AssetId.ToString(network)
                     }).Where(x => x.Address == address).ToArray(),
                 SpentCoins = item.SpentCoins.Select(
                     x => new InputOutput
                     {
-                        Address = x.TxOut.ScriptPubKey.GetDestinationAddress(network).ToString(),
+                        Address = x.TxOut.ScriptPubKey.GetDestinationAddress(network)?.ToString(),
                         Amount = (x as ColoredCoin)?.Amount.Quantity ?? ((Coin)x).Amount.Satoshi,
-                        BcnAssetId = (x as ColoredCoin)?.AssetId.ToString()
+                        BcnAssetId = (x as ColoredCoin)?.AssetId.ToString(network)
                     }).Where(x => x.Address == address).ToArray()
             };
         }
