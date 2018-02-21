ee6fc35e597ab1e6ffdc2024135cd2e25f949416
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-02-21 16:57:04 +0300

    Refactoring: removed unused code.

--- src/Lykke.Job.TxDetector.Core/Domain/BitCoin/IBcnClientCredentialsRepository.cs
+++ src/Lykke.Job.TxDetector.Core/Domain/BitCoin/IBcnClientCredentialsRepository.cs
@@ -13,36 +13,8 @@ namespace Lykke.Job.TxDetector.Core.Domain.BitCoin
         string AssetAddress { get; set; }
     }
 
-    public class BcnCredentialsRecord : IBcnCredentialsRecord
-    {
-        public string Address { get; set; }
-        public string EncodedKey { get; set; }
-        public string PublicKey { get; set; }
-        public string ClientId { get; set; }
-        public string AssetAddress { get; set; }
-        public string AssetId { get; set; }
-
-        public static BcnCredentialsRecord Create(string assetId, string clientId, string address, string assetAddress, string pubKey, string encodedKey = null)
-        {
-            return new BcnCredentialsRecord
-            {
-                Address = address,
-                AssetAddress = assetAddress,
-                AssetId = assetId,
-                ClientId = clientId,
-                EncodedKey = encodedKey,
-                PublicKey = pubKey
-            };
-        }
-    }
-
     public interface IBcnClientCredentialsRepository
     {
-        Task SaveAsync(IBcnCredentialsRecord credsRecord);
-        Task<IBcnCredentialsRecord> GetAsync(string clientId, string assetId);
-        Task<IBcnCredentialsRecord> GetByAssetAddressAsync(string assetAddress);
-        Task<IEnumerable<IBcnCredentialsRecord>> GetAsync(string clientId);
-        Task<string> GetClientAddress(string clientId);
         Task<IEnumerable<IBcnCredentialsRecord>> GetAllAsync(string assetId);
     }
 }
