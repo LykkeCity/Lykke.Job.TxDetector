b25ebb578bc150aaea9a3281bd2a933dd20ce0da
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-02-21 16:50:31 +0300

    Updated nuget package Lykke.Service.Assets.Client + safe refactoring.

--- src/Lykke.Job.TxDetector.Core/Services/BitCoin/IQBitNinjaApiCaller.cs
+++ src/Lykke.Job.TxDetector.Core/Services/BitCoin/IQBitNinjaApiCaller.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using QBitNinja.Client.Models;
 
 namespace Lykke.Job.TxDetector.Core.Services.BitCoin
